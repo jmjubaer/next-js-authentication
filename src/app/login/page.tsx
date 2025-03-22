61313cb1707a93be25846ce37fef87fd9af11113
jmjubaer <jmjubaer3927@gmail.com>
2025-03-22 10:26:00 +0600

    initilize next auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ const LoginPage = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    // formState: { errors },
   } = useForm<FormValues>();
 
   const onSubmit = async (data: FormValues) => {
@@ -65,7 +65,7 @@ const LoginPage = () => {
                 id="password"
                 type="password"
                 {...register("password")}
-                placeholder="Email"
+                placeholder="Password . . . . "
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm  sm:text-sm"
                 required
               />
